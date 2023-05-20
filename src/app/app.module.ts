4743acfd6b581083e3c0ce87882056a90104f5fc
Matt <Mvjarrett92@gmail.com>
2023-05-19 21:44:08 -0400

    remove angular mat library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,7 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { ContainerComponent } from './components/container/container.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-import { MatButtonModule } from '@angular/material/button';
+
 
 
 
@@ -26,11 +22,6 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatDividerModule,
-    MatExpansionModule,
-    CdkAccordionModule,
-    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
